05d3974a1523f0d40f1fe63efcb9debd4dd90916
lymanjohnson <lyman.johnson@gmail.com>
2017-09-27 09:13:22 -0400

    Adds sessions routes, as well as directing the root to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
     resources :posts
     resources :answers
 
+    root to: 'sessions#new'
+    resource :session, only: [:new, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
