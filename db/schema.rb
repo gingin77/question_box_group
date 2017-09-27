adb33ab5027daf2768ffe7c4d976be11ed74c4fe
gingin77 <ginniehench@gmail.com>
2017-09-27 13:27:50 -0400

    I removed the posts-answers join table because it was not necessary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927031858) do
+ActiveRecord::Schema.define(version: 20170927172424) do
 
   create_table "answers", force: :cascade do |t|
     t.string "user"
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20170927031858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_answers_on_post_id"
-  end
-
-  create_table "answers_posts", id: false, force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "answer_id", null: false
-    t.index ["answer_id", "post_id"], name: "index_answers_posts_on_answer_id_and_post_id"
-    t.index ["post_id", "answer_id"], name: "index_answers_posts_on_post_id_and_answer_id", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
