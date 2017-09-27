fb27831632979eb80ec531c2e7c813b36e0a21be
lymanjohnson <lyman.johnson@gmail.com>
2017-09-27 09:46:21 -0400

    Removes unnecessary new.html.erb copy from /views/ folder. Login page loads properly, but login & authentication is still broken.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception  #=> NOTE: This does not work in an API controller.
+  # protect_from_forgery with: :exception  #=> NOTE: This does not work in an API controller.
   #TODO: See if there is some other thing that must be done instead to protect from forgery, or if this is even necessary.
 
   private #=> NOTE: Does this line belong in an API controller??
@@ -7,13 +7,13 @@ class ApplicationController < ActionController::Base
 
   #NOTE: There are two current_user methods below. The first is basic, the second is more complex but better, avoiding unnecessary database calls.
 
-  def current_user #=> NOTE: Does this line belong in an API controller??
-    User.find(session[:user_id]) if session[:user_id]
-  end
-
   # def current_user #=> NOTE: Does this line belong in an API controller??
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #   User.find(session[:user_id]) if session[:user_id]
   # end
+
+  def current_user #=> NOTE: Does this line belong in an API controller??
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   helper_method :current_user #=> NOTE: Does this line belong in an API controller??
 
