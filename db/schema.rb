83fc8d7511e543cbbd90b5764370562cbb01c2a7
gingin77 <ginniehench@gmail.com>
2017-09-26 20:35:47 -0400

    I updated the Question Controller methods. At present, index and show have been modified to use render json: followed by the appropriate instance variable, @posts and @post, respectively. I made and tested methods for create, update, and destroy.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926184235) do
+ActiveRecord::Schema.define(version: 20170926203844) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "user"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "topic"
