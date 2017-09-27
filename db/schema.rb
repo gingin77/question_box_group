1779618988b759a2f05394b3287eac85c1fc54c6
lymanjohnson <lyman.johnson@gmail.com>
2017-09-27 16:09:45 -0400

    Adds create.json.jbuilder view (probably unreliable), and auto-generated test & schema files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927124143) do
+ActiveRecord::Schema.define(version: 20170927145648) do
 
   create_table "answers", force: :cascade do |t|
     t.string "user"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20170927124143) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["token"], name: "index_users_on_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
