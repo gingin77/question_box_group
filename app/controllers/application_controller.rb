35b4e4ebc60122e2c09e341af54c2e12a9631356
lymanjohnson <lyman.johnson@gmail.com>
2017-09-27 09:08:30 -0400

    Adds (but leaves commented out) a more sophisticated current_user method. To be clear, the added method is still commented out, and the current_user method that is actually being used is the older one.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::API
 
   private #=> NOTE: Does this line belong in an API controller??
 
+
+  #NOTE: There are two current_user methods below. The first is basic, the second is more complex but better, avoiding unnecessary database calls.
+
   def current_user #=> NOTE: Does this line belong in an API controller??
     User.find(session[:user_id]) if session[:user_id]
   end
+
+  # def current_user #=> NOTE: Does this line belong in an API controller??
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 
   helper_method :current_user #=> NOTE: Does this line belong in an API controller??
 
