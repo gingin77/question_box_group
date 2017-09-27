96617643f5903d7870ece2b30852566ce08283b4
lymanjohnson <lyman.johnson@gmail.com>
2017-09-27 09:05:23 -0400

    Changes application_controller with session protection methods. Unclear how much of it is appropriate to API controllers. The newly added methods and lines are clearly marked with NOTE tags.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery with: :exception  #=> NOTE: Does this line belong in an API controller??
+
+  private #=> NOTE: Does this line belong in an API controller??
+
+  def current_user #=> NOTE: Does this line belong in an API controller??
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user #=> NOTE: Does this line belong in an API controller??
+
 end
