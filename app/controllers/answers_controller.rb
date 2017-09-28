ba1f26de4093821696cc14bd98c8ac72782f8c6d
knapptime9 <knappj84@yahoo.com>
2017-09-28 17:51:55 -0400

    Main build of answersController complete, may still need some debugging

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,64 @@
 class AnswersController < ApplicationController
+  before_action :require_login, only: %i[new edit create update destroy]
+   before_action :is_owner, only: %i[edit update destroy]
+
+  def index
+    @answers = Answer.all
+    # render 'index.json'
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+    # render 'show.json'
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def edit
+    @answer = Answer.finds(params[:id])
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    @answer.user_id = current_user.id if current_user
+    if @answer.save
+        render json: @answer, status: :created, location: @answer
+    else
+      render json: @answer.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+
+    if @answer.update(answer_params)
+      redirect_to @answer
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    redirect_to answers_path
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:title, :body, :user_id, :link)
+  end
+
+  def require_login
+    redirect_to   new_session_path unless logged_in
+  end
+
+  def is_owner
+    @answer = Answer.find(params[:id])
+    redirect_to answers_path unless @answer.user_id == current_user
+  end
+
 end
