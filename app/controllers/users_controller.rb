62b73f7485aa9b661e1ee165cc48625031c0e5d4
gingin77 <ginniehench@gmail.com>
2017-09-28 11:14:35 -0400

    I've made changes to the User controller. I added an update action, which 'works' in the sense that an update happends. However, I get an error returned...I think I may know why

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def login
     user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
 
