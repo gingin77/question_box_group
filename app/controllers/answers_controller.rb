cbdb5721c32ce0d39e99cc705accfb477190a792
gingin77 <ginniehench@gmail.com>
2017-09-28 20:22:04 -0400

    I was able to delete a post from user 3 after adding the token associated with user 3. Without the token, the message I received was 'bad credentials

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,51 @@
 class AnswersController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+  before_action :authenticate, only: [:create, :update, :destroy]
+
+  def index
+    @posts = Post.all
+    render 'index.json'
+  end
+
+  def show
+    render 'show.json'
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = @user
+    # byebug
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+     @post.destroy
+   end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:topic, :body)
+    end
+
+    # def post_owner
+    #   set_post
+    #   redirect_to books_path unless @post.user_id == current_user.id
+    # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928184536) do
+ActiveRecord::Schema.define(version: 20170928205015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 20170928184536) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_answers_on_post_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -46,5 +48,6 @@ ActiveRecord::Schema.define(version: 20170928184536) do
   end
 
   add_foreign_key "answers", "posts"
+  add_foreign_key "answers", "users"
   add_foreign_key "posts", "users"
 end
